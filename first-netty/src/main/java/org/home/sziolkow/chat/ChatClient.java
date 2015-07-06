34780a2d03916552f2ce05f720fdb2776bb941ea
Slawek <sziolkow@gmail.com>
2015-07-06 07:58:34 +0200

    initial commit

---
+++ first-netty/src/main/java/org/home/sziolkow/chat/ChatClient.java
@@ -0,0 +1,38 @@
+package org.home.sziolkow.chat;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.channels.Channel;
+import java.util.logging.ConsoleHandler;
+
+/**
+ * Created by slawek on 06/07/15.
+ */
+public class ChatClient {
+
+    private final String host;
+    private final int port;
+
+    public ChatClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap().
+                    group(eventLoopGroup).channel(NioSocketChannel.class).handler(new ChatClientInitializer());
+                Channel channel = bootstrap.connect(host, port).sync().channel();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        }
+
+    }
+}
