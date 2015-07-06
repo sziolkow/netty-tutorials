a04a69644d096537d261f9f8ae2fb06033b088b2
Slawek <sziolkow@gmail.com>
2015-07-07 00:08:26 +0200

    trying to send byte arrays

--- netty-streams/src/main/java/org/home/sziolkow/chat/ChatServerInitializer.java
+++ netty-streams/src/main/java/org/home/sziolkow/chat/ChatServerInitializer.java
@@ -1,14 +1,11 @@
 package org.home.sziolkow.chat;
 
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.bytes.ByteArrayDecoder;
+import io.netty.handler.codec.bytes.ByteArrayEncoder;
 
 
 /**
@@ -16,11 +13,12 @@ import io.netty.handler.codec.string.StringEncoder;
  */
 public class ChatServerInitializer extends ChannelInitializer<SocketChannel> {
 
-    @Override protected void initChannel(SocketChannel socketChannel) throws Exception {
-        ChannelPipeline pipeline =  socketChannel.pipeline();
-        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
-        pipeline.addLast("decoder", new StringDecoder());
-        pipeline.addLast("encoder", new StringEncoder());
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("framer", new LengthFieldBasedFrameDecoder(100, 0,2,0,2));
+        pipeline.addLast("decoder", new ByteArrayDecoder());
+        pipeline.addLast("encoder", new ByteArrayEncoder());
         pipeline.addLast("handler", new ChatServerHandler());
     }
 }
